dfa4c44878c9166cf88256034ea7c27c784ed5e0
riemetoba <riemehasan06@gmail.com>
2025-10-08 08:51:41 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// var start here 
+var x = "I'm Okay."
+var x = "I'm fine"
+var x = 63
+
+console.log(x);
+
+// var end here
+
+// let start here 
+let w = "sunny day"
+w="rainy day"
+
+
+console.log(w);
+// let end here 
+
+// const start here 
+const food="beef"
+const pi=3.1416
+
+console.log(food);
+console.log(pi);
+// const end here 
+
+// scope start here 
+{
+    let a = 20
+    const c = 1.5
+
+    console.log(a,c);
+}
+var b = "hello"
+console.log(b);
+// scope end here 
+// scope end he
+
+// let start here 
+// let end here 
